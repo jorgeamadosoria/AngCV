3163001e73db08a5d18a9b9d613a05922ca4b5a6
jorgeamadosoria <darksoul.uci@gmail.com>
2017-12-18 00:31:56 -0300

    reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormArray, FormBuilder, Validators } from '@angular/forms';
+import { CV } from './cv';
+import { CVService } from './cv.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  form: FormGroup;
+  cv: CV;
+  constructor(private formBuilder: FormBuilder, private service: CVService) { }
+
+    getCV(): void {
+      this.service.getCV().subscribe(cv => this.cv = cv);
+    }
+
+    ngOnInit() {
+      this.getCV();
+
+      this.form = this.formBuilder.group({
+          name: ['', [Validators.required, Validators.minLength(5)]],
+          languages: this.formBuilder.array([
+              this.initLanguage()
+          ])
+      });
+  }
+
+  initLanguage() {
+      return this.formBuilder.group({
+          language: ['', Validators.required],
+          level: ['', Validators.required]
+      });
+  }
+
+  addLanguage() {
+      const control = <FormArray>this.form.controls['languages'];
+      control.push(this.initLanguage());
+  }
+
+  removeLanguage(i: number) {
+      const control = <FormArray>this.form.controls['languages'];
+      control.removeAt(i);
+  }
+
+  save(model: CV) {
+      // call API to save
+      // ...
+      console.log(model);
+  }
 }
