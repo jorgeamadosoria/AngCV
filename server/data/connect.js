f193f182627649441553bcb6219e7329350407f6
jorgeamadosoria <darksoul.uci@gmail.com>
2018-05-04 22:15:54 -0300

    configured openshift environment variables for mongdb

--- server/data/connect.js
+++ server/data/connect.js
@@ -1,12 +1,18 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-var promise = mongoose.connect('mongodb://localhost:27017/angcv', {
-    useMongoClient: true,
-});
+var mongodb_connection_string = 'mongodb://127.0.0.1:27017/angcv';
+//take advantage of openshift env vars when available:
+if(process.env.OPENSHIFT_MONGODB_DB_URL){
+  mongodb_connection_string = process.env.OPENSHIFT_MONGODB_DB_URL + process.env.MONGODB_DATABASE;
+}
 
-promise.then(function(db) {
-    db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+var promise = mongoose.connect(mongodb_connection_string, {
+    useMongoClient: true,
+  });
+
+promise.then(function (db) {
+  db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 });
 
 module.exports = mongoose;
