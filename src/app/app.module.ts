f40cee0ecd156f7bd9334d156a3e1178abde8152
Jorge Soria <jsoria@MacBook-Pro.nearsoft.local>
2019-12-30 16:13:56 -0600

    updated dependencies and removed HttpModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
@@ -76,7 +75,6 @@ const appRoutes: Routes = [
       appRoutes,
       { enableTracing: true } // <-- debugging purposes only
     ),
-    HttpModule,
     HttpClientModule,
     BrowserModule,
     MatAutocompleteModule,
